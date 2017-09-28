e43064f6459ffbeac15a31936257bd5e3fbe32ce
kati-frantz <frantzenterprises777@gmail.com>
2017-09-28 08:30:18 +0400

    link

--- resources/views/admin/series/all.blade.php
+++ resources/views/admin/series/all.blade.php
@@ -32,7 +32,7 @@
                 <tbody>
                     @forelse($series as $s)
                         <tr>
-                            <td>{{ $s->title }}</td>
+                            <td><a href="{{ route('series.show', $s->slug) }}">{{ $s->title }}</a></td>
                             <td>
                                 <a href="{{ route('series.edit', $s->slug) }}" class="btn btn-info">Edit</a>
                             </td>
